41ecfe4d5e12ea7341376d0a324b3eab91695c27
Swapnil Karambelkar <6348670+TutorKidsDad@users.noreply.github.com>
2024-09-21 17:17:23 +0530

    Update DHT Sensor Web Server.ino

--- ESP8266 ESP32 DHT Sensor Web Server/DHT Sensor Web Server.ino
+++ ESP8266 ESP32 DHT Sensor Web Server/DHT Sensor Web Server.ino
@@ -1,1 +1,100 @@
+#if defined(ESP8266)
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#elif defined(ESP32)
+#include <WiFi.h>
+#include <WebServer.h>
+#endif
 
+#include "DHT.h"
+#define DHTTYPE DHT22
+
+const char* ssid = "testing";
+const char* password = "password";
+
+#if defined(ESP8266)
+ESP8266WebServer server(80);
+const uint8_t DHTPin = D3;  // Use appropriate pin for ESP8266
+#elif defined(ESP32)
+WebServer server(80);
+const uint8_t DHTPin = 15;  // Use appropriate pin for ESP32
+#endif
+
+// Initialize DHT sensor.
+DHT dht(DHTPin, DHTTYPE);
+
+float Temperature;
+float Humidity;
+
+void setup() {
+  Serial.begin(115200);
+  delay(100);
+
+  pinMode(DHTPin, INPUT);
+
+  dht.begin();
+
+  Serial.println("Connecting to ");
+  Serial.println(ssid);
+
+  // Connect to your local Wi-Fi network
+  WiFi.begin(ssid, password);
+
+  // Check Wi-Fi connection status
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected..!");
+  Serial.print("Got IP: ");
+  Serial.println(WiFi.localIP());
+
+  server.on("/", handle_OnConnect);
+  server.onNotFound(handle_NotFound);
+
+  server.begin();
+  Serial.println("HTTP server started");
+}
+
+void loop() {
+  server.handleClient();
+}
+
+void handle_OnConnect() {
+  Temperature = dht.readTemperature();
+  Humidity = dht.readHumidity();
+  server.send(200, "text/html", SendHTML(Temperature, Humidity));
+}
+
+void handle_NotFound() {
+  server.send(404, "text/plain", "Not found");
+}
+
+String SendHTML(float Temperaturestat, float Humiditystat) {
+  String ptr = "<!DOCTYPE html> <html>\n";
+  ptr += "<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\">\n";
+  ptr += "<title>ESP Weather Report</title>\n";
+  ptr += "<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}\n";
+  ptr += "body{margin-top: 50px;} h1 {color: #444444;margin: 50px auto 30px;}\n";
+  ptr += "p {font-size: 24px;color: #444444;margin-bottom: 10px;}\n";
+  ptr += "</style>\n";
+  ptr += "</head>\n";
+  ptr += "<body>\n";
+  ptr += "<div id=\"webpage\">\n";
+  ptr += "<h1>Weather Display Station</h1>\n";
+
+  ptr += "<p>Temperature is: ";
+  ptr += (int)Temperaturestat;
+  ptr += "°C</p>";
+
+  ptr += "<p>Humidity is: ";
+  ptr += (int)Humiditystat;
+  ptr += "%</p>";
+
+  ptr += "</div>\n";
+  ptr += "</body>\n";
+  ptr += "</html>\n";
+  return ptr;
+}
